0277ff8d952a7ceaca92f1c0e260ff4b2cc5acc9
MadHowl <madhowl@yandex.ru>
2024-10-03 14:07:56 +1000

    blog-with-use-markdown

---
+++ template/main.php
@@ -0,0 +1,39 @@
+<main class="main">
+    <!-- Page Title -->
+    <!-- End Page Title -->
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8">
+                <!-- Blog Details Section -->
+                <section id="blog-details" class="blog-details section">
+                    <div class="container">
+                        <article class="article">
+                            <div class="post-img"><!-- Post image -->
+                                <img src="assets/img/blog/blog-1.jpg" alt="" class="img-fluid">
+                            </div>
+                            <h2 class="title"></h2><!-- Post title -->
+                            <div class="content"><!-- Start post content -->
+
+
+                            </div><!-- End post content -->
+                        </article>
+                    </div>
+                </section>
+                <!-- /Blog Details Section -->
+
+
+            </div>
+
+            <div class="col-lg-4 sidebar">
+
+                <div class="widgets-container">
+                    <!-- widgets -->
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+</main><?php
---
+++ template/partials/widgets/comment_form.php
@@ -0,0 +1,35 @@
+<!-- Comment Form Section -->
+<section id="comment-form" class="comment-form section">
+    <div class="container">
+
+        <form action="">
+
+            <h4>Post Comment</h4>
+            <p>Your email address will not be published. Required fields are marked * </p>
+            <div class="row">
+                <div class="col-md-6 form-group">
+                    <input name="name" type="text" class="form-control" placeholder="Your Name*">
+                </div>
+                <div class="col-md-6 form-group">
+                    <input name="email" type="text" class="form-control" placeholder="Your Email*">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col form-group">
+                    <input name="website" type="text" class="form-control" placeholder="Your Website">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col form-group">
+                    <textarea name="comment" class="form-control" placeholder="Your Comment*"></textarea>
+                </div>
+            </div>
+
+            <div class="text-center">
+                <button type="submit" class="btn btn-primary">Post Comment</button>
+            </div>
+
+        </form>
+
+    </div>
+</section><!-- /Comment Form Section -->
